fa5689126b1fd1a8bb8c051365f2da3b7bdedf75
brayanjoelrv091 <si2psicologiaproy@gmail.com>
2026-06-11 00:05:48 -0400

    chore(frontend): configure file replacements for prod and dev environments

--- frontend/src/app/environment.prod.ts
+++ frontend/src/app/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000',
+  production: true,
+  apiUrl: 'https://rutaigeoproxi-back.onrender.com',
   stripePublishableKey: 'pk_test_51Tf02d39MXEwCbAgEKd2CDqPJ8LBLJxJKrjcEdtZHpska6SUYOpnhhJf4CI36I9ucsRLfEcVSKr6C8f8t2T3idGW00V9AwHL6n'
 };
